1a880e6eb70777afe444821ce7774c6a284dc5c0
Level.Z <1171567018@qq.com>
2018-12-21 11:08:38 +0800

    Create 14.decorator

--- 14.decorator.ts
+++ 14.decorator.ts
@@ -37,10 +37,15 @@ class User2 {
 // 缺点：新增函数validateName入侵到了changeName的逻辑中，破坏了开闭原则
 // 改进方案：装饰器
 
+
+// 在此处target为User3，propertyKey为changeName，descriptor里装着函数changeName的描述
 function validateName(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-  console.log('target:', target);
-  console.log('propertyKey:', propertyKey);
-  console.log('PropertyDescriptor:', descriptor);
+  // 我们重写changeName方法
+  let method = descriptor.value;
+  descriptor.value = function (name: string) {
+    if (name) method.call(this, name);
+    else throw new Error('name is invalid');
+  }
 }
 
 class User3 {
@@ -59,4 +64,73 @@ class User3 {
 }
 
 let user3 = new User3(3, 'Level.Z');
-user3.changeName('a')
+// user3.changeName('')
+
+// 总结：这里我们可以看到，changeName的逻辑没有任何改变，但其实它的行为已经通过装饰器@validateName得到增强
+
+
+// 装饰器在TypeScript中有4中装饰形式，注意：装饰器能装饰在类、方法、属性和参数上，但不能只装饰在函数上
+
+// 类装饰器
+function sealed(constructor: Function) {
+  // 封闭对象，阻止添加新属性并将现有属性标记为不可配置
+  Object.seal(constructor);
+  Object.seal(constructor.prototype);
+} 
+
+@sealed
+class User4 {}
+
+// 方法装饰器
+// 开篇我们讲解的validateName就是方法装饰器，下面我们换种方式，用装饰器工厂来讲解一下方法装饰器
+
+// 装饰器工厂
+function Factory(name: string) {
+  console.log('Factory:', name);
+
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    // DoSomething
+  }
+}
+
+class User5 {
+
+  @Factory('user5_1')
+  getAge() {};
+
+  @Factory('user5_2')
+  getName() {};
+}
+
+let user5 = new User5();
+user5.getAge();
+user5.getName();
+
+// 这里需要注意的是普通装饰器和装饰器工程的区别，普通装饰器只需要函数名称即可
+
+// 属性装饰器
+function name(target: any, propertyKey: string) {}
+
+class User6 {
+  @name
+  name: string;
+}
+
+// 参数装饰器，参数装饰器表达式会在运行时当做函数被调用
+function required(target: any, propertyKey: string, parameterIndex: number) {
+  console.log(target);
+  console.log(propertyKey);
+  console.log(parameterIndex);
+}
+
+class User7 {
+  private name: string;
+  setName(@required name: string) {
+    this.name = name;
+  }
+}
+
+let user7 = new User7();
+user7.setName('Level.Z');
+
+export {};
--- dist/14.decorator.js
+++ dist/14.decorator.js
@@ -6,6 +6,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 class User1 {
     constructor(id, name) {
         this.id = id;
@@ -32,10 +36,16 @@ class User2 {
 }
 // 缺点：新增函数validateName入侵到了changeName的逻辑中，破坏了开闭原则
 // 改进方案：装饰器
+// 在此处target为User3，propertyKey为changeName，descriptor里装着函数changeName的描述
 function validateName(target, propertyKey, descriptor) {
-    console.log('target:', target);
-    console.log('propertyKey:', propertyKey);
-    console.log('PropertyDescriptor:', descriptor);
+    // 我们重写changeName方法
+    let method = descriptor.value;
+    descriptor.value = function (name) {
+        if (name)
+            method.call(this, name);
+        else
+            throw new Error('name is invalid');
+    };
 }
 class User3 {
     constructor(id, name) {
@@ -50,4 +60,65 @@ __decorate([
     validateName
 ], User3.prototype, "changeName", null);
 let user3 = new User3(3, 'Level.Z');
-user3.changeName('a');
+// user3.changeName('')
+// 总结：这里我们可以看到，changeName的逻辑没有任何改变，但其实它的行为已经通过装饰器@validateName得到增强
+// 装饰器在TypeScript中有4中装饰形式，注意：装饰器能装饰在类、方法、属性和参数上，但不能只装饰在函数上
+// 类装饰器
+function sealed(constructor) {
+    // 封闭对象，阻止添加新属性并将现有属性标记为不可配置
+    Object.seal(constructor);
+    Object.seal(constructor.prototype);
+}
+let User4 = class User4 {
+};
+User4 = __decorate([
+    sealed
+], User4);
+// 方法装饰器
+// 开篇我们讲解的validateName就是方法装饰器，下面我们换种方式，用装饰器工厂来讲解一下方法装饰器
+// 装饰器工厂
+function Factory(name) {
+    console.log('Factory:', name);
+    return function (target, propertyKey, descriptor) {
+        // DoSomething
+    };
+}
+class User5 {
+    getAge() { }
+    ;
+    getName() { }
+    ;
+}
+__decorate([
+    Factory('user5_1')
+], User5.prototype, "getAge", null);
+__decorate([
+    Factory('user5_2')
+], User5.prototype, "getName", null);
+let user5 = new User5();
+user5.getAge();
+user5.getName();
+// 这里需要注意的是普通装饰器和装饰器工程的区别，普通装饰器只需要函数名称即可
+// 属性装饰器
+function name(target, propertyKey) { }
+class User6 {
+}
+__decorate([
+    name
+], User6.prototype, "name", void 0);
+// 参数装饰器，参数装饰器表达式会在运行时当做函数被调用
+function required(target, propertyKey, parameterIndex) {
+    console.log(target);
+    console.log(propertyKey);
+    console.log(parameterIndex);
+}
+class User7 {
+    setName(name) {
+        this.name = name;
+    }
+}
+__decorate([
+    __param(0, required)
+], User7.prototype, "setName", null);
+let user7 = new User7();
+user7.setName('Level.Z');
